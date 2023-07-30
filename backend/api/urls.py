a1a760b7e563f528dad4761d21babde51564f00f
Evgenia Tarasova <etarasova@live.ru>
2023-07-30 14:33:02 -0700

    Fixed flake8 in urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (AddAndDeleteSubscribe, AddDeleteFavoriteRecipe,
-                       AddDeleteShoppingCart, AuthToken, 
-                       IngredientsViewSet, RecipesViewSet, 
-                       TagsViewSet, UsersViewSet, 
+                       AddDeleteShoppingCart, AuthToken,
+                       IngredientsViewSet, RecipesViewSet,
+                       TagsViewSet, UsersViewSet,
                        set_password)
 
 app_name = 'api'
