12a4780ac0b313e81fe0aec8a75c2b016776d2ba
Evgenia Tarasova <etarasova@live.ru>
2023-07-30 15:01:53 -0700

    Urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
           name='shopping_cart'),
      path('', include(router.urls)),
      path('', include('djoser.urls')),
-     path('auth/', include('djoser.urls.')),
+     path('auth/', include('djoser.urls.authtoken')),
 ]
