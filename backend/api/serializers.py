d2496adb94bb57b5f8cd193050093ab1342d367e
Evgenia Tarasova <etarasova@live.ru>
2023-07-30 13:13:02 -0700

    Fixed flake8

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -308,12 +308,3 @@ class SubscribeSerializer(serializers.ModelSerializer):
         return SubscribeRecipeSerializer(
             recipes,
             many=True).data
-    """
-{
-    "image": "https://yandex.ru/images/search?from=tabbar&img_url=https%3A%2F%2Fi.stack.imgur.com%2FJf1GQ.png&lr=43&pos=0&rpt=simage&text=%D0%BA%D0%B0%D0%BA%20%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D0%BE%20%D0%B2%20%D0%B7%D0%B0%D0%B3%D0%BE%D0%BB%D0%BE%D0%B2%D0%BA%D0%B5%20Authorization%20%D0%BF%D0%B5%D1%80%D0%B5%D0%B4%D0%B0%D0%B2%D0%B0%D1%82%D1%8C%20%D1%82%D0%BE%D0%BA%D0%B5%D0%BD",
-    "ingredients": [{"id": 1, "amount": 100}]
-    "tags": "",
-    "name": "test_01",
-    "text": "hekko",
-    "cooking_time": "10"
-}"""
